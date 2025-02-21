d61bcad6c665716a68777ec69897ec854043c97a
milan <milancakic89@gmail.com>
2025-02-21 21:29:51 +0100

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'transactions',
+        component: TransactionsComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'auth',
+    },
+];
